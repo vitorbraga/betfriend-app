ea2281bd2fde2d3bdf2c27909be885995332f9fb
vitorbr <vitorbr@motorola.com>
2016-10-13 16:59:15 -0300

    Server changed

--- app/src/main/java/br/com/betfriend/utils/Constants.java
+++ app/src/main/java/br/com/betfriend/utils/Constants.java
@@ -13,8 +13,8 @@ public class Constants {
 
     public static final String SOCCER_API_BASE_URI = "https://arisalexis-soccer-odds-v1.p.mashape.com/";
 
-//    public static final String SERVER_API_BASE_URI = "http://www.betfriendapp.com/";
-    public static final String SERVER_API_BASE_URI = "http://10.10.198.136:3000/";
+	public static final String SERVER_API_BASE_URI = "http://www.betfriendapp.com/";
+    //public static final String SERVER_API_BASE_URI = "http://10.10.198.136:3000/";
 
     public static final String SERVER_KEY = "KEY-178268-SERVER-cxXRWlBhws9p1aT8zjjsn";
 }
